2958f4e4ce77b05fc9dae7f1683ad95d5b2bad95
romarionijim <romario.nijim@growthspace.com>
2025-02-06 10:03:59 +0200

    feat: add mock payloads for created and updated pets in API helpers

---
+++ infra/api/helpers/mocks/payloads.ts
@@ -0,0 +1,38 @@
+import MockDataGenerator from "@api-helpers";
+
+export const payloads = {
+  
+  createdPet: {
+    id: 3193,
+    category: {
+      id: MockDataGenerator.getRandomLongNumber(),
+      name: MockDataGenerator.getRandomName()
+    },
+    name: 'Pikachu',
+    photoUrls: ['https://ibb.co/wLWCrSX'],
+    tags: [
+      {
+        id: MockDataGenerator.getRandomLongNumber(),
+        name: MockDataGenerator.getRandomName(),
+      }
+    ],
+    status: 'available'
+  },
+
+  updatePet: {
+    id: 3193,
+    category: {
+      id: MockDataGenerator.getRandomLongNumber(),
+      name: MockDataGenerator.getRandomName()
+    },
+    name: 'Pokey',
+    photoUrls: ['https://ibb.co/0Z9v02Z'],
+    tags: [
+      {
+        id: MockDataGenerator.getRandomLongNumber(),
+        name: MockDataGenerator.getRandomName(),
+      }
+    ],
+    status: 'available'
+  }
+}
